db506a01b1de1aca8066ba7611f9cdbc1fd3fe80
KIDwolf <732398507@qq.com>
2021-05-14 20:10:01 +0800

    完成基础布局

--- game-mgr-fe/src/router/index.js
+++ game-mgr-fe/src/router/index.js
@@ -6,12 +6,20 @@ const routes = [
     name: 'Auth',
     component: () => import(/* webpackChunkName:"auth" */'../views/Auth/index.vue'),
   },
-  //{
-  //path: '/about',
-  // name: 'About',
 
-  // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  //},
+  {
+    path: '/',
+    name: 'BasicLayout',
+    component: () => import(/* webpackChunkName:"BasicLayout" */'../layout/BasicLayout/index.vue'),
+    children: [
+      {
+        path: '/games',
+        name: 'Games',
+        component: () => import(/* webpackChunkName:"Games" */'../views/Games/index.vue'),
+      },
+    ],
+  },
+
 ];
 
 const router = createRouter({
---
+++ game-mgr-fe/src/views/Games/index.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+games
+</div>
+</template>
